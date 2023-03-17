f0e8293d468e2e6f320bdad7997420c6cd284d01
Null <kelanidarasimi9@gmail.com>
2023-03-17 16:34:50 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriptions: print lowercase letter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+ch = 'a';
+
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}

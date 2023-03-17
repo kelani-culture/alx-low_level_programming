f884fbbc9d86d624190a526f06c8f03821e28bf0
Null <kelanidarasimi9@gmail.com>
2023-03-16 18:39:28 -0700

    Do not print output using printf/puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 as success 
+ */
+
+int main()
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}

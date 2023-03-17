f90c232ade5b42ca2d01e8e0a9dad5e3aaa370eb
Null <kelanidarasimi9@gmail.com>
2023-03-16 18:49:45 -0700

    fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+
+0;276;0c#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }

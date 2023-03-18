93bdcb4aed6790914cca7e8b6208ca69ab82e53f
Null <kelanidarasimi9@gmail.com>
2023-03-17 17:49:16 -0700

    letter in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - tired
+ *
+ * Return: Always  0 (success)
+ */
+
+int main(void)
+{
+char ch; 
+ch ='z';
+
+for (ch ='z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}

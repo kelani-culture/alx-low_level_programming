049714ad51acecaad287c44258141a37244bbaad
Null <kelanidarasimi9@gmail.com>
2023-03-17 17:11:27 -0700

    print integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n;
+n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return 0;
+}

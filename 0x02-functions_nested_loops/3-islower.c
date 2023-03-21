388fb8bb73ad3c90d9c9ea030b055403dd20c0b7
Null <kelanidarasimi9@gmail.com>
2023-03-21 14:54:52 -0700

    create a function that return lowercase value

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return (0);
+	}
+}

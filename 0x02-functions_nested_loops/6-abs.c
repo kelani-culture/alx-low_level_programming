d44e4d85b4ef1ac0d5d702bc1f97534fbee8d66e
Null <kelanidarasimi9@gmail.com>
2023-03-21 17:41:04 -0700

    Absolute value always positive

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - main entry point
+ *
+ *@i: absolute value
+ *
+ * Return: Always 0 (success)
+ */
+
+int _abs(int i)
+
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+}
+

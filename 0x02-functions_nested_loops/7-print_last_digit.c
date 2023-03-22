085e9a96bfae544cc547209355a77bb4669b3f25
Null <kelanidarasimi9@gmail.com>
2023-03-22 02:10:13 -0700

    last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - main entry point
+ *
+ * @n: number to be computed
+ * Return: Always 0 (success)
+ */
+
+int  print_last_digit(int, n)
+{
+	last_digit = n % 10;
+	if (n < 0)
+	{
+		return *= 1;
+	}
+	_putchar(last_digit + '0');
+}

3a94f3bf79889c4790cca3e574b0436f5d7d988a
Null <kelanidarasimi9@gmail.com>
2023-03-22 09:23:17 -0700

    fix multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,38 +1,30 @@
 #include "main.h"
 
 /**
- * times_table - print every multiplication starting from 0
- * Return: void
+ * times_table - Prints the 9 times table, starting with 0.
  */
-
 void times_table(void)
 {
-
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
 	{
-		for (mult = 0; mult <= 9; mult++)
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			if (mult != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
+
 			prod = num * mult;
 
 			if (prod <= 9)
-			{
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar((prod / 10) + '0');
-			}
-			_putchar((prod % 10) + '0');
 
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
-
 }

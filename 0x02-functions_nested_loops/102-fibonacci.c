0215a7301f76d6824c4d2f96effa5a6bfe692f86
Null <kelanidarasimi9@gmail.com>
2023-03-22 17:53:42 -0700

    first 50 fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int a = 1, b = 2, c, fibo;
+
+	for (fibo = 0; fibo <= 50; fibo++)
+	{
+		printf("%lu", a);
+		c = a + b;
+		a = b;
+		b = c;
+		if (fibo != 50)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

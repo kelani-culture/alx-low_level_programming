f43b7d89f5085bc23a1725a27e83e7e3a56707b2
Null <kelanidarasimi9@gmail.com>
2023-03-22 19:01:38 -0700

    print fibonacci 97 fib seq

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (success)
+ */
+
+int main(void)
+{
+	long int prev, curr, next_term;
+	int i;
+
+	for (i = 0; i < 98; i++)
+	{
+		printf("%lu", prev);
+
+		next_term = prev + curr;
+
+		prev = curr;
+		curr == next_term;
+
+		if (i != (98 - 1))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

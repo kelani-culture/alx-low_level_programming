7f0976994bf456f7e4c16c8fe3bfa3d26804ac37
Null <kelanidarasimi9@gmail.com>
2023-03-23 11:41:19 -0700

    print up to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,20 +8,18 @@
 
 void more_numbers(void)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (i <= 10)
+	for (i = 0; i <= 10; i++)
 	{
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 9)
 			{
-				_putchar('0' + (j / 10);
+				_putchar((j / 10) + '0');
 			}
-			_putchar('0' + (j % 10));
-			j++;
+			_putchar((j % 10) + '0');
 		}
-		i++;
+		_putchar('\n');
 	}
 }

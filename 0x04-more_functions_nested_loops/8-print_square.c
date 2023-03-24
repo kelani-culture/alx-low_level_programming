74aba505aac3b103517f8b732689388710934af7
Null <kelanidarasimi9@gmail.com>
2023-03-24 05:20:48 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - print square 
+ * @size: integers
+ *
+ * Return: void
+ */
+
+
+void print_square(int size)
+{
+	int r, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (r = 1; r <= size; r++)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

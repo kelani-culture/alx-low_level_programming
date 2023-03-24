365d493a9cc79ef83e2cae07e94e8c96f80c073a
Null <kelanidarasimi9@gmail.com>
2023-03-24 14:38:09 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - ptint triangle using octothorpe
+ *
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		_putchar('\n');
+	}
+
+	for (x = 0; x <= size; x++)
+	{
+		for (space == size - x; space > 0; space--)
+		{
+			_putchar(' ');
+		}
+		for (y = 0; y <= x; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

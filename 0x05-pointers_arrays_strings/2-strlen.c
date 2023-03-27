1107ec00f5476d4bdc7ac4b923da4e9aece675ec
Null <kelanidarasimi9@gmail.com>
2023-03-27 14:30:41 -0700

    a function that prints a string, followed by a new line, to

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen -  returns the length of a string.
+ * _strlen - main entry points
  *
+ * Descriptions: return the length of a string
  * @s: string
- * return: length of string
+ *
+ * return: the lenth of string s
  */
 
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		i++;

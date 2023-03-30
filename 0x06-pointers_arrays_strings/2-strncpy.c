60724e2bfd3c99ceb33d9db07ef7374e3b27a90b
Null <kelanidarasimi9@gmail.com>
2023-03-30 12:52:20 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ *
+ * @src: source char
+ * @dest: destination char
+ * @n: bytes size
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}

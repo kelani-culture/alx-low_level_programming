9aff7d9f927f444e1970a9c7d5a749d893a4e03c
Null <kelanidarasimi9@gmail.com>
2023-03-30 15:18:56 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare the difference between two string
+ *
+ * @s1: string
+ * @s2: string3
+ * Return: differnce betweween string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (s1 - s2);
+}

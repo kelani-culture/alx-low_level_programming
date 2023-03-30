13b3b16b4bc993843f1bdc3436c4b71d0e9af68f
Null <kelanidarasimi9@gmail.com>
2023-03-29 23:43:40 -0700

    This function appends the src string to the dest string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings together
+ *
+ * @src: strings to concatenate from
+ * @dest: string to contatenate to
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+
+		*ptr++;
+		*src++;
+	}
+	*ptr = '\0';
+	return (dest);
+}

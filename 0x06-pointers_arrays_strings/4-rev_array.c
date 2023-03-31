aa1fba3e3c61665a0bede977c920796bdf7ebf75
Null <kelanidarasimi9@gmail.com>
2023-03-31 00:19:45 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse value in an array
+ *
+ * @a: pointer integer
+ * @n: size of array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *start_ptr = a;
+	int *end_ptr = a + n - 1; /* the first array added to the size of the the array -1 gives the last element of the array*/
+	int temp;
+
+	while (end_ptr > start_ptr)
+	{
+		temp = *start_ptr;
+		*start_ptr = end_ptr;
+		*end_ptr = temp;
+
+		start_ptr++;
+		end_ptr--;
+	}
+}

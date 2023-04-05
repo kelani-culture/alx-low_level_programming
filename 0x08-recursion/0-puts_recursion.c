1eb2f821f3f1290429aa5236a17654b6abf9ab8c
Null <kelanidarasimi9@gmail.com>
2023-04-05 05:46:13 -0700

    do not understand checker issue

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\n')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (*s == '\n')
 	{
 		_putchar('\n');
 		return;
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }

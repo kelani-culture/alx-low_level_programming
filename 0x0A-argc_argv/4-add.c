a62e42c21167552037b7053cb729ca3819a95743
Null <kelanidarasimi9@gmail.com>
2023-04-07 18:55:39 -0700

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * add_positive - add only positive number
+ *
+ * @argc: integer value
+ * @argv: char array
+ */
+
+int add_positive(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]) || atoi(argv[i]) < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+
+/**
+ * main - Entry points
+ *
+ * @argc: argument counts
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	add_positive(argc, argv);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,4 +3,5 @@
 
 int main(int argc, char *argv[]);
 int _multi(int argc, char *argv[]);
+int add_positive(int argc, char *argv[]);
 #endif

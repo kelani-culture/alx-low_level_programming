d7f538abb8e668397b29011c8b6b91e013957e9a
Null <kelanidarasimi9@gmail.com>
2023-04-11 03:19:49 -0700

    duplicate string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function that duplicate a string
+ *
+ * @str: string to duplicate
+ * Return: chr
+ */
+int strL(char *str);
+
+char *_strdup(char *str)
+{
+	char *chr;
+	int i, len = strL(str);
+
+	if (str == NULL)
+		return (NULL);
+
+	chr = (char *)malloc(len * sizeof(char));
+
+	if (chr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0;  i <= len; i++)
+		chr[i] = str[i];
+
+	return (chr);
+}
+
+/**
+ * strL - calculate length of string
+ *
+ * @str: string to count
+ * Return: length of string
+ */
+
+int strL(char *str)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + strL(s + 1));
+}

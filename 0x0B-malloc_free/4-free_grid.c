6327de431cd590075be65cf05f630b07417fc192
Null <kelanidarasimi9@gmail.com>
2023-04-11 09:21:54 -0700

    deallocate 2D array from memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a 2 dimensional grid previously
+ *
+ * @grid: integer
+ * @height: integer
+ * Return: freed grid
+ */
+
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	return (free(grid));
+}

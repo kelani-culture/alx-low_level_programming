12a6e7c4f5bb779047b9231a21fe72b29e5e80d5
Null <kelanidarasimi9@gmail.com>
2023-04-12 08:46:17 -0700

    fix program

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,75 +11,34 @@
  * Return: chr
  */
 
-int len(char *s1);
-char *_strcat(char *s1, char *s2);
-
 char *str_concat(char *s1, char *s2)
 {
+	int len, i, j = 0, len = 0;
 	char *chr;
-	unsigned int i, len1, len2;
 
 	if (s1 == NULL)
-	{
-		s1 = (char *)malloc(sizeof(char));
-		s1[0] = '\0';
-	}
-	if (s2 == NULL)
-	{
-		s2 = (char *)malloc(sizeof(char));
-		s2[0] = '\0';
-	}
-	len1 = len(s1);
-	len2 = len(s2);
+		s1 = "";
 
-	chr = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; i < s1[i] || s2[i]; i++)
+		len++;
+
+	chr = (char *)malloc((len + 1) * sizeof(char));
 
 	if (chr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	for (i = 0; i < s1[i]; i++)
 	{
-		chr[i] = s1[i];
+		chr[j++] = s1[i];
 	}
-	_strcat(chr, s2);
+
+	for (i = 0; i < s2[i]; i++)
+	{
+		chr[j++] = s2[i];
+	}
+
 	return (chr);
-}
-
-/**
- * len -  count length
- * @s1: string to count
- *
- * Return: length of string
- */
-
-int len(char *s1)
-{
-	if (*s1 == '\0')
-	{
-		return (0);
-	}
-
-	else
-	{
-		return (1 + len(s1 + 1));
-	}
-}
-
-/**
- * _strcat - concat two strings
- *
- * @s1: destination to concat to
- * @s1: to append from
- * Return: dest
- */
-
-char *_strcat(char *s1, char *s2)
-{
-	int dest_len = len(s1), i;
-
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		s1[dest_len + i] = s2[i];
-	}
-	return (s1);
 }

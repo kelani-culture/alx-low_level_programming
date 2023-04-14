c6a8607c2ab3d4def309cb5f61e72857fe52147c
Null <kelanidarasimi9@gmail.com>
2023-04-14 03:26:11 -0700

    fix check 10

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,14 +13,14 @@ int *array_range(int min, int max)
 {
 	int *alloc;
 	unsigned int i;
-	size_t size = (max - min);
+	size_t size = (max - min) + 1;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	alloc = malloc(sizeof(int) * size + sizeof(int));
+	alloc = malloc((sizeof(int) * size) + sizeof(int));
 	if (alloc == NULL)
 		return (NULL);
 

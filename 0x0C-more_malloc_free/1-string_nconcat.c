1fe65b8823ab42807ad9b86076d1921462d4f8de
Null <kelanidarasimi9@gmail.com>
2023-04-14 02:26:44 -0700

    fix compile  issue

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int len = 0, index = 0, len_2 = 0, j;
-	int i;
+	unsigned int len = 0, len_2 = 0, index = 0;
+	unsigned int i;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,11 +24,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	/* length of both strings sum together */
-	for (i = 0; i < s1[i] || s2[i]; i++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
 	/* length of string s2 */
-	for (i = 0; i < s2[i]; i++)
+	for (i = 0; s2[i]; i++)
 		len_2++;
 
 	concat = malloc(sizeof(char) * len);
@@ -36,17 +36,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		concat[index++] = s1[i];
-	}
 
-	for (j = 1; j <= n; j++)
+
+	for (i = 1; i <= n; i++)
 	{
-		concat[index++] = s2[j - 1];
+		concat[index++] = s2[i - 1];
 
-		if (s2[j] != '\0')
+		if (s2[i] == '\0')
 			break;
 	}
-	concat[index] = '\0';
 	return (concat);
 }

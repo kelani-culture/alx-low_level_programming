153d9af77c543b7947c80d6534bbe3729c66bcb1
Null <kelanidarasimi9@gmail.com>
2023-04-14 03:12:44 -0700

    fix check 6

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,29 +23,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	/* length of both strings sum together */
-	for (i = 0; s1[i] || s2[i]; i++)
+	for (i = 0; s1[i]; i++)
 		len++;
-
-	/* length of string s2 */
-	for (i = 0; s2[i]; i++)
+	for (i = 0; s2[i] && i <  n; i++)
 		len_2++;
 
-	concat = malloc(sizeof(char) * len -  1);
+	concat = malloc(sizeof(char) * (len + len_2 + 1));
 	if (concat == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		concat[index++] = s1[i];
 
+	for (i = 0; s2[i] && i < n; i++)
+		concat[index++] = s2[i];
 
-	for (i = 1; i <= n; i++)
-	{
-		concat[index++] = s2[i - 1];
-
-		if (s2[i] == '\0')
-			break;
-	}
 	concat[index] = '\0';
 	return (concat);
 }

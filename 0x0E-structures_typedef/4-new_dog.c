b4c9123a4cce92e3c0a1ff7a0ceaf29846ed89a0
Null <kelanidarasimi9@gmail.com>
2023-04-18 07:02:17 -0700

    dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/*
+ * new_dog - a copy of name and owner
+ *
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Return: dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = malloc(sizeof(dog));
+
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+
+	return (dog);
+}

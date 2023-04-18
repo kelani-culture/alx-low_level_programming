9d9ca0327f4abe86d59d2f6fdd03acdce19094f8
Null <kelanidarasimi9@gmail.com>
2023-04-18 11:55:14 -0700

    check if *d is NULL

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,12 +3,15 @@
 
 /**
  * free_dog - function that frees dogs
- * 
+ *
  * @d: dog_t
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
 	free(d->name);
 	free(d->owner);
 	free(d);

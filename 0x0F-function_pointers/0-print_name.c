a694bda1401abe9cc75c19b2b2c8f3f868a2dc70
Null <kelanidarasimi9@gmail.com>
2023-04-19 07:42:21 -0700

    fix checker error

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - function that print name
  *
@@ -10,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (*name != '\0' && name != NULL && f != NULL)
 		f(name);
 }

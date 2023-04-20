ca677a5659736704d4d5329414e75b827f24e7b4
Null <kelanidarasimi9@gmail.com>
2023-04-20 01:42:19 -0700

    calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i]).op == *s)
+		if (*s == *(ops[i]).op)
 		{
 			return (ops[i].f);
 		}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 /**
  * main - Entry point
  *
@@ -27,12 +27,12 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if ((argv[2] == "/" && atoi(argv[3]) == 0) ||
-		(argv[2] == "%" && atoi(argv[3]) == 0))
+	if ((strcmp(argv[2], "/") == 0 && atoi(argv[3]) == 0) ||
+		(strcmp(argv[2], "%") == 0 && atoi(argv[3]) == 0))
 	{
 		printf("ERROR\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[2])));
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }

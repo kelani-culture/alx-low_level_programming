f86d0270ca8e7b75c97bf73969866ec32266c8fa
Null <kelanidarasimi9@gmail.com>
2023-04-20 01:53:07 -0700

    fix checker

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -15,9 +15,10 @@ int main(int argc, char *argv[]);
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 #endif

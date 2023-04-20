a6ae1b9b3b7e30cb7963e3973ea97d756ff91396
Null <kelanidarasimi9@gmail.com>
2023-04-20 16:47:20 -0700

    fix error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,13 +17,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			str = va_arg(args, char *);
+		str = va_arg(args, char *);
+		if (separator != NULL)
 			printf("%s%s", str, i < n - 1 ? separator : "\n");
-		}
 	}
 	va_end(args);
 }

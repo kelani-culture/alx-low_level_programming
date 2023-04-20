bcc8eabf4e4b505a907def3f1b5e842563e98bdf
Null <kelanidarasimi9@gmail.com>
2023-04-20 16:07:07 -0700

    fix checker

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,8 +22,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			str = va_arg(args, char *);
-			printf("%s%s", str, i < n - 1 ? separator : "\n");
+			if (str != '\0')
+			{
+				printf("%s",n);
+			}
+			else
+			{
+				if ( i < n - 1)
+					printf("%s", separator);
+			}
 		}
+		printf("\n");
 	}
 	va_end(args);
 }

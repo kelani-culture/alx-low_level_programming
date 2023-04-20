d6c26f541a79bd93a4b350c932a9390ee8766995
Null <kelanidarasimi9@gmail.com>
2023-04-20 16:44:24 -0700

    fix error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,18 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			x = va_arg(args, int);
-			printf("%d", x);
-
-			if (i < n - 1)
-				printf("%s", separator);
-		}
-		printf("\n");
+		x = va_arg(args, int);
+		printf("%d", x);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(args);
 }

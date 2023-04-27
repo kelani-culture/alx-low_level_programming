08a1fb963ec7b270cc3faa14077f1380fe5ab3a0
Null <kelanidarasimi9@gmail.com>
2023-04-27 10:31:07 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * list_len - return length of the linked list
+ *
+ * @h: pointer to the linked list
+ * Return: count
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}

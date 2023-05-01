5ff876673d017566b3cbe8ff4aecbb17d5bff569
Null <kelanidarasimi9@gmail.com>
2023-05-01 01:36:47 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - free all allocated list
+ *
+ * @head: pointer to head node
+ */
+
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	free(head);
+}

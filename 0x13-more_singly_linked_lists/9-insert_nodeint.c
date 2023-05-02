b4dac433dc14f768f4dbd6505091fc99d2abfda4
Null <kelanidarasimi9@gmail.com>
2023-05-02 02:53:13 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,12 +34,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	currNode = *head;
 	if (idx == 0)
 	{
-		newNode->next = currNode;
-		currNode = newNode;
-		return (currNode);
+		newNode->next = *head;
+		*head = newNode;
+		return (*head);
 	}
 
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx; i++)
 	{
 		currNode = currNode->next;
 	}

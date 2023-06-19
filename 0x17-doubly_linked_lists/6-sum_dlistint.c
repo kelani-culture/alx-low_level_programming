571e75aea4bb19209eaaa44b19bf68b9d63c8836
Null <kelanidarasimi9@gmail.com>
2023-06-19 06:01:59 -0700

    sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_dlistint - sum linked list
+ * @head: pointer to head node
+ * Return: sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *head_ref = head;
+
+	if (!head)
+		return (0);
+	
+	while (head_ref != NULL)
+	{
+		sum += head_ref->n;
+		head_ref = head_ref->next;
+	}
+	return (sum);
+}

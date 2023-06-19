5aa5caaa7474241296ba71b17758f2b0b4646f9c
Null <kelanidarasimi9@gmail.com>
2023-06-19 12:11:03 -0700

    insert at a givned index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index( - insert at a given index
+ * @h: pointer to head node
+ * @idx: index of node
+ * @n: data to insert
+ * Return: linked list
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *head_ref = *h, *temp, *newNode;
+	unsigned int len = 0, i;
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	while (head_ref != NULL)
+	{
+		head_ref = head_ref->next;
+		len++;
+	}
+	newNode->n = n;
+	if (idx > len - 1)
+		return (NULL);
+	head_ref = *h;
+	if (idx == 0)
+		add_dnodeint(h, n);
+	else if (idx == len - 1)
+		add_dnodeint_end(h, n);
+	else
+	{
+		i = 1;
+		while (i < idx)
+		{
+			head_ref = head_ref->next;
+			i++;
+		}
+		temp = head_ref->next;
+		newNode->prev = head_ref;
+		newNode->next = head_ref->next;
+		head_ref->next = newNode;
+		temp->prev = newNode;
+	}
+	return (*h);
+}

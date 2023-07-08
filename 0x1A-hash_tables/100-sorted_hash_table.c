39b44223c21a1cc3bfa7ee767cb39b81feee225b
Null <kelanidarasimi9@gmail.com>
2023-07-08 01:53:20 -0700

    add string.h header file

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+#include <string.h>
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);

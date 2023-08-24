a206a771dffaeea729a2d304e964b0b7e0e380fd
Null <kelanidarasimi9@gmail.com>
2023-08-24 08:13:35 +0100

    fix pycodesttyle

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,23 @@
 """"
     calculate perimeters of the island described in grid
 """
+
+
 def island_perimeter(grid):
     """
         calculate perimeter
     """
     perimeter = 0
-    
     rows = len(grid)
     cols = len(grid[0])
-    
+
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:  # Check if it's a land cell
                 perimeter += 4  # Assume 4 edges
-                
                 # Check neighboring cells (up, down, left, right)
                 if row > 0 and grid[row - 1][col] == 1:
                     perimeter -= 2  # Deduct 2 for shared edge
                 if col > 0 and grid[row][col - 1] == 1:
                     perimeter -= 2  # Deduct 2 for shared edge
-                    
     return perimeter

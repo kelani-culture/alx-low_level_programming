ac6a010bfa3324de8c77ea36f5138e3a51f6ea11
Null <kelanidarasimi9@gmail.com>
2024-01-16 16:53:43 +0100

    task 0 redid

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,10 +12,10 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t i;
     for (i = 0; i < size; i++) {
+        printf("Value checked array[%ld] = [%d]\n",i, array[i]);
         if (array[i] == value) {
             return array[i];
         }
-        printf("Value checked array[%ld] = [%d]\n",i, array[i]);
     }
     return (-1);
 }

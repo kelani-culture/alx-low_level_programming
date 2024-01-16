457829f2e934c36e07dbe228e6229112f6c18621
Null <kelanidarasimi9@gmail.com>
2024-01-16 18:02:40 +0100

    task 1 redid

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -15,7 +15,7 @@ int binary_search(int *array, size_t size, int value)
 		return (-1);
 	while (low < high)
 	{
-		printf("searching in array: ");
+		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
 			if (i == high)

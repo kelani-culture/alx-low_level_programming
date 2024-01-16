24ff0adc5206781e3fa0d48d793d38c653d343d7
Null <kelanidarasimi9@gmail.com>
2024-01-16 16:51:23 +0100

    task 1 linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * linear_search - a function that performs linear search
+ * @array: pointer to the first element in the array
+ * @size: size of the array
+ * @value: value to search for
+ * Return: value if found else return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i;
+    for (i = 0; i < size; i++) {
+        if (array[i] == value) {
+            return array[i];
+        }
+        printf("Value checked array[%ld] = [%d]\n",i, array[i]);
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,4 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+int linear_search(int *array, size_t size, int value);
+#endif
